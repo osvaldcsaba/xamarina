0dce447f09f0546cdf828d6dac70c2b18d443fd3
Osvald Csaba <csaba.osvald@hotmail.com>
2016-08-29 17:34:54 +0200

    OK

--- masodfoku/masodfoku/Resources/Resource.Designer.cs
+++ masodfoku/masodfoku/Resources/Resource.Designer.cs
@@ -63,26 +63,26 @@ namespace masodfoku
 			// aapt resource value: 0x7f050003
 			public const int a_ertek = 2131034115;
 			
-			// aapt resource value: 0x7f050006
-			public const int b_ertek = 2131034118;
+			// aapt resource value: 0x7f050004
+			public const int b_ertek = 2131034116;
 			
-			// aapt resource value: 0x7f05000b
-			public const int btnSzamol = 2131034123;
+			// aapt resource value: 0x7f05000a
+			public const int btnSzamol = 2131034122;
 			
-			// aapt resource value: 0x7f050009
-			public const int c_ertek = 2131034121;
+			// aapt resource value: 0x7f050007
+			public const int c_ertek = 2131034119;
 			
 			// aapt resource value: 0x7f050001
 			public const int linearLayout1 = 2131034113;
 			
-			// aapt resource value: 0x7f050004
-			public const int linearLayout2 = 2131034116;
+			// aapt resource value: 0x7f050005
+			public const int linearLayout2 = 2131034117;
 			
-			// aapt resource value: 0x7f050007
-			public const int linearLayout3 = 2131034119;
+			// aapt resource value: 0x7f050008
+			public const int linearLayout3 = 2131034120;
 			
-			// aapt resource value: 0x7f05000a
-			public const int linearLayout4 = 2131034122;
+			// aapt resource value: 0x7f05000b
+			public const int linearLayout4 = 2131034123;
 			
 			// aapt resource value: 0x7f05000c
 			public const int linearLayout5 = 2131034124;
@@ -93,11 +93,11 @@ namespace masodfoku
 			// aapt resource value: 0x7f050002
 			public const int textView2 = 2131034114;
 			
-			// aapt resource value: 0x7f050005
-			public const int textView3 = 2131034117;
+			// aapt resource value: 0x7f050006
+			public const int textView3 = 2131034118;
 			
-			// aapt resource value: 0x7f050008
-			public const int textView4 = 2131034120;
+			// aapt resource value: 0x7f050009
+			public const int textView4 = 2131034121;
 			
 			// aapt resource value: 0x7f05000d
 			public const int txtEredmeny = 2131034125;
