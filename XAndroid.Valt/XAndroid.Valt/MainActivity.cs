8d4c195fe4504cbf80b875454d72b256baf95130
Osvald Csaba <csaba.osvald@hotmail.com>
2016-09-07 17:55:43 +0200

    OK

---
+++ XAndroid.Valt/XAndroid.Valt/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XAndroid.Valt
+{
+    [Activity(Label = "@string/ApplicationName", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.MainButton);
+
+            button.Click += delegate 
+            {
+                Intent intent = new Intent(this, typeof(MasodikActivity));
+                this.StartActivity(intent);
+            };
+        }
+    }
+}
+
---
+++ XAndroid.Valt/XAndroid.Valt/MasodikActivity.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XAndroid.Valt
+{
+    [Activity(Label = "Masodik Activity")]
+    public class MasodikActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.MasodikLayout);
+
+            var button = FindViewById<Button>(Resource.Id.MasodikButton);
+            button.Click += (sender, e) =>
+            {
+                this.Finish();
+            };
+        }
+
+    }
+}
