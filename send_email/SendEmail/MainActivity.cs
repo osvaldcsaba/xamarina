7e2a2bfcce7f81d092ab971d0d3c9b3aa3f0b92f
Osvald Csaba <csaba.osvald@hotmail.com>
2017-07-04 12:47:27 +0200

    Felesleges Using-ok eltávolítása.

--- send_email/SendEmail/MainActivity.cs
+++ send_email/SendEmail/MainActivity.cs
@@ -1,9 +1,6 @@
-using System;
 
 using Android.App;
 using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
 
@@ -22,11 +19,11 @@ namespace SendEmail
             
             button.Click += (sender,e) =>
             {              
-                var email = new Intent (Android.Content.Intent.ActionSend);
-                email.PutExtra(Android.Content.Intent.ExtraEmail, "csaba.osvald@gmail.com");
-                email.PutExtra(Android.Content.Intent.ExtraCc, "csaba.osvald@freemail.hu");
-                email.PutExtra(Android.Content.Intent.ExtraSubject, "Próba levél");
-                email.PutExtra(Android.Content.Intent.ExtraText, "Szia! Ez egy próba levél szövege.");
+                var email = new Intent (Intent.ActionSend);
+                email.PutExtra(Intent.ExtraEmail, "csaba.osvald@gmail.com");
+                email.PutExtra(Intent.ExtraCc, "csaba.osvald@freemail.hu");
+                email.PutExtra(Intent.ExtraSubject, "Próba levél");
+                email.PutExtra(Intent.ExtraText, "Szia! Ez egy próba levél szövege.");
                 email.SetType("message/rfc822");
                 StartActivity(email);
             };
